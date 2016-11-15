2d4fe27b5c576f2c423a1eacd05c6db3bf851d18
rails <quantv@live.com>
2016-11-15 16:16:41 +0700

    update example, separate upload url

--- sample/routes/elfinder.js
+++ sample/routes/elfinder.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var el = require('../../elfinder-connector'),
-    path = require('path'),
-    promise = require('promise'),
-    multer = require('multer'),
-    fs = require('fs-extra'),
+  path = require('path'),
+  promise = require('promise'),
+  multer = require('multer'),
+  fs = require('fs-extra'),
   _ = require('underscore');
 /* GET users listing. */
 var media = path.resolve('media');
@@ -17,13 +17,13 @@ el.setup({
     path.join(media, 'protected'),
     path.join(media, 'private', user),
   ],
-  init: function(){
+  init: function () {
     //create protected and private folder.
-    if (!fs.existsSync(path.join(media, 'protected', user))){
-        fs.mkdirSync(path.join(media, 'protected', user));
+    if (!fs.existsSync(path.join(media, 'protected', user))) {
+      fs.mkdirSync(path.join(media, 'protected', user));
     }
-    if (!fs.existsSync(path.join(media, 'private', user))){
-        fs.mkdirSync(path.join(media, 'private', user));
+    if (!fs.existsSync(path.join(media, 'private', user))) {
+      fs.mkdirSync(path.join(media, 'private', user));
     }
   }
   //acl: function(user, path){}
@@ -31,40 +31,42 @@ el.setup({
 
 router.get('/', function (req, res, next) {
   var cmd = req.query.cmd;
-  if (cmd == 'file'){
+  if (cmd == 'file') {
     var target = el.decode(req.query.target);
     res.sendFile(target.absolutePath);
-  }else if (el[cmd]){
-    el[cmd](user, req.query).then(function(result){
+  } else if (el[cmd]) {
+    el[cmd](user, req.query).then(function (result) {
       res.json(result);
-    }).catch(function(e){
-      console.log(e);
-      res.json({error: e.message});
+    }).catch(function (e) {
+      res.json({ error: e.message });
     })
   }
-
 });
-var upload = multer({dest: 'media/.tmp/'});
-router.post('/', function(req, res, next){
-  try{
-    var up = upload.array('upload[]', 10);
-    up(req, res, function(err){
-      if (err){
-        return res.json({error: err})
-      }
-      el.upload(user, req.body, req.files).then(function(result){
-        res.json(result);
-      }).catch(function(e){
-        res.json({error: e.message});
-      });
+
+router.post('/', function (req, res, next) {
+  var cmd = req.body.cmd;
+  if (cmd && el[cmd]) {
+    el[cmd](user, req.body).then(function (result) {
+      res.json(result);
+    }).catch(function (e) {
+      res.json({ error: e.message });
     })
-  }catch(e){
-    res.json({error: 'Not implemented'});
   }
 })
+
+var upload = multer({ dest: 'media/.tmp/' });
+router.post('/upload', upload.array('upload[]', 10), function(req, res, next){
+    el.upload(user, req.body, req.files).then(function (result) {
+      res.json(result);
+    }).catch(function (e) {
+      res.json({ error: e.message });
+    });
+})
+
 router.get('/tmb/:filename', function (req, res, next) {
   res.sendFile(el.tmbfile(req.params.filename));
 })
+
 router.get('/file/:volume/*', function (req, res, next) {
   var file = el.filepath(req.params.volume, req.params['0']);
   if (file)
